3103d8efa6a0f0473fc2713e0fddba1f6eb7e7d2
AnuramVarma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-07-25 22:19:07 +0530

    Create Arthimatic_Operators.py

---
+++ Programs-1/Arthimatic_Operators.py
@@ -0,0 +1,11 @@
+#Arthimatic Operators
+a=int(input())
+b=int(input())
+
+print("Sum",a+b)
+print("Subtraction",a-b)
+print("Product",a*b)
+print("Division ",a/b)
+print("Exponent",a**b)
+print("Floor Division ",a//b)
+print("Modulus",a%b)
