9c4e40e25973d439563eb2389b43629d597c2ddc
AnuramVarma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-07-29 21:21:28 +0530

    Create Sides_of_a_Triangle.py

---
+++ Programs-2/Sides_of_a_Triangle.py
@@ -0,0 +1,7 @@
+a=int(input("Enter value of a :"))
+b=int(input("Enter value of b :"))
+c=int(input("Enter value of c :"))
+if a+b>c and b+c>a and a+b+c:
+    print("Sides are Equal")
+else:
+    print("It is Invalid !")
