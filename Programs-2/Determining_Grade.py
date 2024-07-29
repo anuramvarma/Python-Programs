0b2cfafef969f62893798e10d740af5ee3fec822
AnuramVarma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-07-29 20:50:19 +0530

    Create Determining_Grade.py

---
+++ Programs-2/Determining_Grade.py
@@ -0,0 +1,14 @@
+a=int(input())
+   if(a>=90):
+     print("GRADE A")
+   elif(a>=80):
+       print("GRADE B")
+   elif(a>=70):
+       print("GRADE C")
+   elif(a>=60):
+       print("GRADE D")
+   elif(a>=50):
+       print("GRADE E")
+   else:
+       print("FAILED")
+  
