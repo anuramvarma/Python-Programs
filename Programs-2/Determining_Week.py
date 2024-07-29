835a50e3f077db12672cc928a3e8de2d1e31903c
AnuramVarma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-07-29 20:58:38 +0530

    Create Determining_Week.py

---
+++ Programs-2/Determining_Week.py
@@ -0,0 +1,15 @@
+a=int(input("Enter value of a: "))
+if (a==1):
+       print("Monday")
+elif (a==2):
+       print("Tuesday")
+elif (a==3):
+       print("Wednesday")   
+elif (a==4):
+       print("Thursday")
+elif (a==5):
+       print("Friday")
+elif (a==6):
+       print("Saturday")
+else:
+    print("Sunday")
