efd54d1dee4c655df81d7c03f287f48b72ec3734
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 14:27:44 +0530

    Create Big_Small_equal.py

---
+++ Hacker_Rank/Python_Begin_2023-1/Big_Small_equal.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+if(a==b):
+    print("Equal")
+elif(a>b):
+    print("Big")
+else:
+    print("Small")
