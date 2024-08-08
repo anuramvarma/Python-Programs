a1bfec8c8cf31cc6260a9cd9baa9dd529d8c9cc2
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-08 14:28:32 +0530

    Create Product_Three_Nums.py

---
+++ Hacker_Rank/Python_Begin_2023-1/Product_Three_Nums.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=-1
+if(a==7):
+    print(b*c)
+elif(b==7):
+    print(c)
+elif(c==7):
+    print(d)
+else:
+    print(a*b*c)
