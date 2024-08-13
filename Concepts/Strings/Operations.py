af89b1b874885d749b8d46c3aee8592ffd185e89
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-13 12:22:14 +0530

    Create Operations.py

---
+++ Concepts/Strings/Operations.py
@@ -0,0 +1,9 @@
+s1="Hello "
+s2="Freinds !!"
+print("Concatenated : ",s1+s2)
+print("Repetion : ",s1*2,s2)
+for i in "Hello Friends !!":
+    print(i)
+print("Membership : ",'F' in s2)
+print("Membership : ",'k' not in s1)
+print(s1[2])
