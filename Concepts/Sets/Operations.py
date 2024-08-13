65acb3667579a1d8d7d177bf7578444098591a22
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-13 11:41:46 +0530

    Create Operations.py

---
+++ Concepts/Sets/Operations.py
@@ -0,0 +1,8 @@
+s1={3,4,56,6,20,33,4,0,2,11}
+s2={3,5,6,1}
+print("Union ",s1|s2)
+print("Intersection ",s1&s2)
+print("Difference  ",s1-s2)
+print("Symmetric Diiference  ",s1^s2)
+print("Membership Operation :",'a' in s1)
+print("Membership Operation :",6 not in s2)
