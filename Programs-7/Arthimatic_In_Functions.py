47f43258f53466be62b25b113cfb783212105210
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-27 10:39:32 +0530

    Create Arthimatic_In_Functions.py

---
+++ Programs-7/Arthimatic_In_Functions.py
@@ -0,0 +1,8 @@
+def Arthimatic(a,b):
+	print("Addition :",a+b)
+	print("Substraction : ",a-b)
+	print("Product : ",a*b)
+	print("Division : ",a/b)
+	print("Modulo Division : ",a%b)
+Arthimatic(2,5)
+
