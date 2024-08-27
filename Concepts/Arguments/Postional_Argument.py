b3e7be3c741afd5a2897fc5742eaf667c9701629
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-27 10:22:32 +0530

    Create Postional_Argument.py

---
+++ Concepts/Arguments/Postional_Argument.py
@@ -0,0 +1,11 @@
+def num(a,b,c):
+	print("The values of a,b,c after function is called :",a,b,c)
+num(36,45,54)
+
+def num(a,b,c):
+	print(a+b/c)
+num(65,96,8)
+
+def num(a,b,c):
+	print(a+b-c/2.0)
+num(5,45,8)
